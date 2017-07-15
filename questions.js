01a3f451b60a5b6aff4268ef4e2e28de1818044e
Sandra <Sandra@Sandras-MacBook-Air.local>
2017-07-15 19:50:12 -0400

    last push

---
+++ questions.js
@@ -0,0 +1,30 @@
+exports.questions = [
+	{
+		full: 'Essential oils can be used on children and infants.',
+		cloze: 'children'
+	},
+	{
+		full: 'The purity of essential oils is the same across brands.',
+		cloze: 'purity'
+	},
+	{
+		full: 'You can ingest essential oils.',
+		cloze: 'ingest'
+	},
+	{
+		full: 'Essential oils don"t expire.',
+		cloze: 'Essential oils'
+	},
+	{
+		full: 'Temperature has no effect on essential oils.',
+		cloze: 'Temperature'
+	},
+	{
+		full: 'Subaru was voted the most reliable mid-size sedan in 2017.',
+		cloze: 'Subaru'
+	},
+	{
+		full: 'Subaru has the best resale, value over Toyota.',
+		cloze: 'Toyota'
+	}
+]
